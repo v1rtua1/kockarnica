0a8c2763080aabe45cd492ecd5340fd8aa1e36a3
BuildTools <unconfigured@null.spigotmc.org>
2025-11-24 16:03:40 +0100

    Update site titles

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,4 +1,10 @@
 import AdminLayout from "@/components/AdminLayout"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: "Admin Panel | Casino Royale",
+    description: "Casino Royale Administration",
+}
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return <AdminLayout>{children}</AdminLayout>
